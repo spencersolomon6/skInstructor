2764a1284493eb865a9d841e25b18d128e7b52ca
spencersolomon <spencersolomon6@gmail.com>
2024-08-22 22:10:53 -0400

    Fixed merge conflicts between the branches

---
+++ backend/poseDetection/src/main.py
@@ -0,0 +1,47 @@
+import os.path
+
+import numpy as np
+
+from util.poseModule import PoseDetector
+import argparse
+import json
+from os import walk
+from util.RNN import RNN
+
+
+def main(args):
+    verbose = args.verbose or False
+    train = args.train or False
+    demo = args.demo or False
+
+    rnn = RNN(verbose=verbose)
+    detector = PoseDetector()
+
+    if demo:
+        detector.displayVideo('data/training_videos/vid1.mp4')
+
+    if train:
+        trainingDir = 'data/training_data'
+        dataFiles = next(walk(trainingDir), ([None, None, []]))[2]
+
+        for filename in dataFiles:
+            batchedData = np.array([])
+            with open(os.path.join(trainingDir, filename)) as f:
+                data = json.load(f)
+                print(data.shape)
+                batchedData = np.append(batchedData, data)
+                print(batchedData)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        prog='skInstructor',
+        description='An AI driven ski instructor',
+        epilog='Go to the Github for more.'
+    )
+
+    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--demo', action='store_true')
+
+    main(parser.parse_args())
--- backend/pose_detection/src/settings.py
+++
@@ -1,6 +0,0 @@
-# Specify model file paths
-
-protoFile = "pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt"
-weightsFile = "pose/mpi/pose_iter_160000.caffemodel"
-
-threshold = .5
